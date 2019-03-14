9c024eda183a6d2f44e7537c7d981a654becccd4
itcast <bos@itcast.cn>
2019-03-14 16:03:02 +0800

    添加放啊

--- git/src/cn/itcast/Git.java
+++ git/src/cn/itcast/Git.java
@@ -5,6 +5,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("git w");
+        System.out.println(111);
     }
 
     public int add(){
